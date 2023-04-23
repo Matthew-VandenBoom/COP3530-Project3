81d5861b150599282482b42130f84fc3e5c2c515
Matthew VandenBoom <MatthewVandenBoom@outlook.com>
2023-04-23 13:36:30 -0400

    initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: all
+
+all:
+	g++ "./src/cpp/*" -o "./Build/project3.exe" -I"./src/include/" 
---
+++ src/include/graph.h
@@ -0,0 +1,10 @@
+#include <string>
+class Graph
+{
+public:
+    // however you want to create the graph, i was thinking a path to the file with the data
+    Graph();
+    Graph(std::string file);
+
+private:
+};
