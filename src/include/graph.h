bac56e6b627d5d187a2cfbb1c83fd8506bac06e0
Matthew VandenBoom <MatthewVandenBoom@outlook.com>
2023-04-26 11:59:52 -0400

    Changed the graph class API so it can be consistent for everyone working with it.

--- src/include/graph.h
+++ src/include/graph.h
@@ -1,10 +1,22 @@
 #include <string>
+
+#include <cstdint>
+#include <unordered_map>
+
 class Graph
 {
 public:
     // however you want to create the graph, i was thinking a path to the file with the data
+    // that was passed to a DataReader object
     Graph();
     Graph(std::string file);
+
+    // returns the number of nodes in the graph
+    int NumNodes();
+
+    // returns the out edges + their weights for the passed node
+    // the actual output container can change, this was just for testing
+    std::unordered_map<uint32_t, uint32_t> GetAdjacent(uint32_t nodeID);
 
 private:
 };
